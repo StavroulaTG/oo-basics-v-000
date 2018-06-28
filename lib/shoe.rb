b87cf2ad60d36ae6826280ab22e84991a909d4e8
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-28 01:11:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,12 +11,6 @@ class Shoe
   def cobble
     puts "Your shoe is as good as new!"
   end
-
-  def new=(new)
-    @new = new
-  end
-  def new
-    @new
-  end
+  puts "Your shoe is as good as new!"
 
 end
